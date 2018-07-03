a0af3bb1c8d34818738c78e52e41588b4d261acd
Rebecca Weinberger <rew@mit.edu>
2018-07-02 21:48:37 -0400

    Print stdev / avg dist

--- reader.py
+++ reader.py
@@ -1,13 +1,17 @@
+import numpy as np
 import h5py
 import os
 import sys
 import tables
 import math
 from datetime import datetime
+from pymongo import MongoClient
+from mongoengine import *
 
-startTime = datetime.now()
-
-data_path = './data_small'
+PORT_NUMBER = 21017
+HOST = 'localhost'
+DATA_PATH = './data_small'
+START_TIME = datetime.now()
 
 def get_dirs(path):
     return [ name for name in os.listdir(path) if os.path.isdir(os.path.join(str(path), name))]
@@ -22,7 +26,7 @@ def get_distance_coordinate(lat_1, long_1, lat_2, long_2):
     d_long= deg_to_rad(long_2 - long_1)
     a = math.sin(d_lat/2) * math.sin(d_lat/2) + math.cos(deg_to_rad(lat_1)) * math.cos(deg_to_rad(lat_2)) * math.sin(d_long/2) * math.sin(d_long/2)
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    d = R * c
+    d = EARTH_RADIUS * c
     return d
 
 def deg_to_rad(deg):
@@ -42,44 +46,69 @@ def progress(count, total, status=''):
 
     percents = round(100.0 * count / float(total), 1)
     bar = '#' * filled_len + '.' * (bar_len - filled_len)
-    time_diff = datetime.now() - startTime
+    time_diff = datetime.now() - START_TIME
     time_str = ' '+str(time_diff).replace(',', '')
     sys.stdout.write("\033[K")
     sys.stdout.write('%s [%s] %s%s   %s\r' % (time_str, bar, percents, '%', status))
     sys.stdout.flush()
 
+def find_file_paths():
+    paths = []
+    root = get_dirs('%s' % DATA_PATH)
 
-all_file_paths = []
-artist_neighbors = {}
+    for h1 in root:
+        first_level = get_dirs('%s/%s' % (DATA_PATH, h1))
+        for h2 in first_level:
+            second_level = get_dirs('%s/%s/%s' % (DATA_PATH, h1, h2))
+            for h3 in second_level:
+                filenames = get_files('%s/%s/%s/%s' % (DATA_PATH, h1, h2, h3))
+                for file in filenames:
+                    path = '%s/%s/%s/%s/%s' % (DATA_PATH, h1, h2, h3, file)
+                    paths.append(path)
+    num_files = len(paths)
+    print('%d files' % num_files)
+    return paths
 
-root = get_dirs('%s' % data_path)
-
-for h1 in root:
-    first_level = get_dirs('%s/%s' % (data_path, h1))
-    for h2 in first_level:
-        second_level = get_dirs('%s/%s/%s' % (data_path, h1, h2))
-        for h3 in second_level:
-            filenames = get_files('%s/%s/%s/%s' % (data_path, h1, h2, h3))
-            for file in filenames:
-                path = '%s/%s/%s/%s/%s' % (data_path, h1, h2, h3, file)
-                all_file_paths.append(path)
-
-num_files = len(all_file_paths)
-print('%d files' % num_files)
-
-unavailable_locations = []
-count = 0
-for p1 in all_file_paths:
-    count += 1
-    lat1, long1, a1 = get_lat_long_artist(p1)
-    if math.isnan(lat1) or math.isnan(long1):
-        unavailable_locations.append(a1)
-        continue
-    for p2 in all_file_paths:
-        lat2, long2, a2 = get_lat_long_artist(p2)
-        if math.isnan(lat2) or math.isnan(long2):
+def populate_db(paths):
+    distances = []
+    num_files = len(paths)
+    artist_neighbors = {}
+    unavailable_locations = []
+    count = 0
+    for p1 in paths:
+        count += 1
+        lat1, long1, a1 = get_lat_long_artist(p1)
+        if math.isnan(lat1) or math.isnan(long1):
+            unavailable_locations.append(a1)
             continue
-    progress(count, num_files, a1)
+        for p2 in paths:
+            lat2, long2, a2 = get_lat_long_artist(p2)
+            if math.isnan(lat2) or math.isnan(long2):
+                continue
+            # print(get_distance_coordinate(lat1, long1, lat2, long2))
+            distances.append(get_distance_coordinate(lat1, long1, lat2, long2))
+        progress(count, num_files, a1)
+    # print('locations unavailable for', unavailable_locations)
+    print('\nElapsed time:',datetime.now() - START_TIME)
+    return distances
 
-# print('locations unavailable for', unavailable_locations)
-print('\nElapsed time:',datetime.now() - startTime)
+# class Song(Document):
+#     title = StringField(required=True, max_length=200)
+#     artist = StringField(required=True)
+#     neighbors = ListField(ReferenceField(Song, reverse_delete_rule=mongoengine.PULL))
+
+if __name__ == "__main__":
+    # client = MongoClient(HOST, PORT_NUMBER)
+    # db = client['music_graph']
+    # connect('music_graph', host=HOST, port=PORT_NUMBER)
+
+    paths = find_file_paths()
+    distances = populate_db(paths)
+    std = np.std(distances)
+    avg = np.average(distances)
+    print(std)
+    print(avg)
+
+
+
+
