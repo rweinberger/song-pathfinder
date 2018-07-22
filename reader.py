657abc3b36a09b4955ca0207141d11c4553f11b3
Rebecca Weinberger <rew@mit.edu>
2018-07-22 00:59:27 -0400

    Build data and add to db

--- reader.py
+++ reader.py
@@ -9,11 +9,12 @@ from datetime import datetime
 from pymongo import MongoClient
 from mongoengine import *
 
-PORT_NUMBER = 21017
+PORT_NUMBER = 27017
 HOST = 'localhost'
 DATA_PATH = './data_small'
 START_TIME = datetime.now()
 MAX_HOP_KM = 500
+CORES = 4
 
 # class Song(Document):
 #     title = StringField(required=True, max_length=200)
@@ -26,8 +27,7 @@ def get_dirs(path):
 def get_files(path):
     return [f for f in os.listdir(path) if f.endswith('.h5')]
 
-def get_distance_coordinate(lat_1, long_1, lat_2, long_2):
-    #haversine formula
+def get_distance_coordinate(lat_1, long_1, lat_2, long_2): # haversine formula
     EARTH_RADIUS = 6371
     d_lat = deg_to_rad(lat_2 - lat_1)
     d_long= deg_to_rad(long_2 - long_1)
@@ -60,6 +60,7 @@ def progress(count, total, status=''):
     sys.stdout.flush()
 
 def find_file_paths():
+    """ Flattens the DATA_PATH directory into a list of all contained file paths. """
     paths = []
     root = get_dirs('%s' % DATA_PATH)
 
@@ -77,18 +78,29 @@ def find_file_paths():
     return paths
 
 def build_data(paths, i_start, i_end):
+    """
+    Builds two dictionaries from the given data paths:
+        artists = {
+            artist_name: { song_name: (lat, long) }, ... }
+        }
+        artist_neighbors = {
+            artist_name: [ ( artist_name, song_name ), ...]
+        }
+    """
     artists = {}
     artist_neighbors = {}
     num_files = len(paths)
-    unavailable_locations = []
     count = 0
+
     for p1 in paths[i_start:i_end]:
         count += 1
         lat1, long1, a1, s1 = get_lat_long_artist_song(p1)
         if math.isnan(lat1) or math.isnan(long1):
-            unavailable_locations.append(a1)
             continue
-        artists[a1] = (a1, s1, lat1, long1)
+        if a1 in artists:
+            artists[a1].update({s1: (lat1, long1)})
+        else:
+            artists[a1] = {s1: (lat1, long1)}
         artist_neighbors[a1] = []
         for p2 in paths:
             if p2 != p1:
@@ -97,20 +109,15 @@ def build_data(paths, i_start, i_end):
                     continue
                 dist = get_distance_coordinate(lat1, long1, lat2, long2)
                 if dist <= MAX_HOP_KM:
-                    add_entry((a1, s1, lat1, long1), (a2, s2, lat2, long2), artist_neighbors)
+                    artist_neighbors[a1].append((a2, s2))
         progress(count, 80, a1)
     # print('locations unavailable for', unavailable_locations)
     # print('\nElapsed time:',datetime.now() - START_TIME)
-    return (artists, artist_neighbors)
+    return [artists, artist_neighbors]
 
-def add_entry(artist1, artist2, artist_neighbors):
-    a1, s1, lat1, long1 = artist1
-    a2, s2, lat2, long2 = artist2
-    artist_neighbors[a1].append(artist2)
-
-def worker(paths, i_start, i_end, send_end):
+def worker(paths, i_start, i_end, send_pipe):
     data = build_data(paths, i_start, i_end)
-    send_end.send(data)
+    send_pipe.send(data)
 
 def find_indices(i, n, c):
     increment = math.ceil(n/c)
@@ -118,19 +125,21 @@ def find_indices(i, n, c):
     end = start + increment
     if end >= n:
         end = None
-    print(i, start,end)
     return (start, end)
 
 def execute_workers(cores, paths):
-    pipes = []
+    send_pipes = []
+    recv_pipes = []
     processes = []
     num_paths = len(paths)
     # split_paths = np.array_split(np.array(paths), cores, axis=0)
     for i in range(cores):
-        pipes.append(multiprocessing.Pipe(False))
+        recv_pipe, send_pipe = multiprocessing.Pipe(False)
+        send_pipes.append(send_pipe)
+        recv_pipes.append(recv_pipe)
     for i in range(cores):
         i_start, i_end = find_indices(i, num_paths, cores)
-        processes.append(multiprocessing.Process(target=worker, args=(paths, i_start, i_end, pipes[i][1])))
+        processes.append(multiprocessing.Process(target=worker, args=(paths, i_start, i_end, send_pipes[i])))
     for process in processes:
         process.start()
     for process in processes:
@@ -139,25 +148,40 @@ def execute_workers(cores, paths):
     artists = {}
     artist_neighbors = {}
 
-    print('\ndone receiving')
-
     for i in range(cores):
-        artists.update(pipes[i][0].recv()[0])
-        artist_neighbors.update(pipes[i][0].recv()[1])
+        new_artists, new_neighbors = recv_pipes[i].recv()
+        artists.update(new_artists)
+        artist_neighbors.update(new_neighbors)
 
     # print(len(result))
     return (artists, artist_neighbors)
 
 if __name__ == "__main__":
-    # client = MongoClient(HOST, PORT_NUMBER)
-    # db = client['music_graph']
-    # connect('music_graph', host=HOST, port=PORT_NUMBER)
-    CORES = 4
+    client = MongoClient(HOST, PORT_NUMBER)
+    db = client.music_graph
+    db.artists.drop()
+    db.neighbors.drop()
+
     paths = find_file_paths()
-    # data = build_data(paths, 0, None)
     artists, artist_neighbors = execute_workers(CORES, paths)
-    print('-------')
-    print(artists)
+
+    print('\ndata scraped')
+
+    for artist in artists:
+        entry = {
+            "artist": artist,
+            "songs": artists[artist]
+        }
+        db.artists.insert(entry)
+
+    for artist in artist_neighbors:
+        entry = {
+            "artist": artist,
+            "neighbors": artist_neighbors[artist]
+        }
+        db.neighbors.insert(entry)
+
+    print('collections filled')
     # distances = build_data(paths)
     # std = np.std(distances)
     # avg = np.average(distances)
